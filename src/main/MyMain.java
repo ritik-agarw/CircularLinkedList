17ed3a454b09a2d9bf3743a7661eac3ac3d3cc4f
agarw <agarwal4041rit@gmial.com>
2021-02-18 11:58:43 +0530

    Circular Linked List

--- src/main/MyMain.java
+++ src/main/MyMain.java
@@ -1,4 +1,18 @@
 package main;
 
+import implementation.MySinglyCircularLinkedList;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MySinglyCircularLinkedList circularLinkedList = new MySinglyCircularLinkedList();
+        circularLinkedList.traverse();
+        System.out.println(circularLinkedList.size());
+        circularLinkedList.addFirst(20);
+        circularLinkedList.addLast(10);
+        circularLinkedList.addLast(30);
+        circularLinkedList.traverse();
+        System.out.println(circularLinkedList.removeFirst());
+        circularLinkedList.traverse();
+        System.out.println(circularLinkedList.size());
+    }
 }
